577592ea85a50b05936c1c4116790bff8c2a0b9e
Time <82890272+KehlanMetor@users.noreply.github.com>
2024-04-11 09:59:26 +0200

    Update okmec.js

--- okmec.js
+++ okmec.js
@@ -1,1 +1,11 @@
-awejifgrekbhnjgaerjbhnkger
+const { SlashCommandBuikder } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+        .setName("okmec")
+        .setDescription("tvq mec"),
+
+  run: async ({ interaction }) => {
+    
+  }
+}
